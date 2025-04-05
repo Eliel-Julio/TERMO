80a2f58a87d57babca018deca1cc75f8eef6c123
Eliel-Julio <elieljuliosoaresdasilva@gmail.com>
2025-04-05 02:04:58 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var tentativa = 0
+var tentativa = 1
 var palavra_chave = sprt_palavra('caixa')
 
 function sprt_palavra(str){
@@ -11,24 +11,79 @@ function sprt_palavra(str){
     ]
     return palavra_vet
 }
-function add_letra(){
-
+function contem(elemeto,vetor){
+    for (const i in vetor) {
+        if(i==elemeto){
+            return true
+            break
+    }
+    return false
+}
+function cont(ele,vet){
+    var x = contem(ele,vet)
+    window.alert(x)
+}
 }
 function definir_palavra(){
+    if(tentativa>5){
+        window.alert("tentativas esgotadas")
+    }
     var palavra = window.prompt('escolha a palavra')
     if (palavra.length!=5){
-        window.alert('palavra não uniforme')
+        window.alert('palavra não conforme')
     }else{
-        // palavra_vetor=sprt_palavra(palavra)
-        // var linha=document.getElementById('line'+Text(tentativa))
+        palavra_vetor=sprt_palavra(palavra)
+        {//atribuindo as letras no html  
+        var l1 = document.getElementById('letter1_'+tentativa)
+            l1.innerHTML=palavra_vetor[0]
+        var l2 =document.getElementById('letter2_'+tentativa)
+            l2.innerHTML=palavra_vetor[1]
+        var l3 =document.getElementById('letter3_'+tentativa)
+            l3.innerHTML=palavra_vetor[2]
+        var l4 =document.getElementById('letter4_'+tentativa)
+            l4.innerHTML=palavra_vetor[3]
+        var l5 =document.getElementById('letter5_'+tentativa)
+            l5.innerHTML=palavra_vetor[4]
+        }
+        {
+            if(contem(palavra_vetor[0],palavra_chave)){
+                l1.className="letter s_right"
+            }else if(palavra_vetor[0]==palavra_chave[0]){
+                l1.className="letter right"
+            }else{
+                l1.className="letter rong"
+            }
+            if(contem(palavra_vetor[1],palavra_chave)){
+                l2.className="letter s_right"
+            }else if(palavra_vetor[1]==palavra_chave[1]){
+                l2.className="letter right"
+            }else{
+                l2.className="letter rong"
+            }
+            if(contem(palavra_vetor[2],palavra_chave)){
+                l3.className="letter s_right"
+            }else if(palavra_vetor[2]==palavra_chave[2]){
+                l3.className="letter right"
+            }else{
+                l3.className="letter rong"
+            }
+            if(contem(palavra_vetor[3],palavra_chave)){
+                l4.className="letter s_right"
+            }else if(palavra_vetor[3]==palavra_chave[3]){
+                l4.className="letter right"
+            }else{
+                l4.className="letter rong"
+            }
+            if(contem(palavra_vetor[4],palavra_chave)){
+                l5.className="letter s_right"
+            }else if(palavra_vetor[4]==palavra_chave[4]){
+                l5.className="letter right"
+            }else{
+                l5.className="letter rong"
+            }
+        }
         
-        window.alert('line'+tentativa)
         tentativa++
-        linha.getElementById("letter"+"1_"+tentativa).Text=palavra_vetor[0]
-        linha.getElementById("letter"+"2_"+tentativa)palavra_vetor[1]
-        linha.getElementById('letter'+"3_"+tentativa)palavra_vetor[2]
-        linha.getElementById('letter'+"4_"+tentativa)palavra_vetor[3]
-        linha.getElementById('letter'+"5_"+tentativa)palavra_vetor[4]
-        // add_letra()
+        
     }
 }
