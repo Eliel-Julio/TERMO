0e171d4afdb2182833aa03eeba3ecc07ab990de5
Eliel-Julio <elieljuliosoaresdasilva@gmail.com>
2025-04-06 17:26:18 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var tentativa = 1
-var palavra_chave = 'caixa'
+var palavra_chave = 'sagaz'
 
 function sprt_palavra(str){
     palavra_vet = [
@@ -21,72 +21,100 @@ function contem(elemeto,vetor){
 }
 }
 function definir_palavra(){
-    if(tentativa>5){
+    if(tentativa>6){
         window.alert("tentativas esgotadas")
-    }
-    var palavra = window.prompt('escolha a palavra')
-    if (palavra.length!=5){
-        window.alert('palavra não conforme')
     }else{
-        palavra_vetor=sprt_palavra(palavra)
-        {//atribuindo as letras no html  
-        var l1 = document.getElementById('letter1_'+tentativa)
-            l1.innerHTML=palavra_vetor[0]
-        var l2 =document.getElementById('letter2_'+tentativa)
-            l2.innerHTML=palavra_vetor[1]
-        var l3 =document.getElementById('letter3_'+tentativa)
-            l3.innerHTML=palavra_vetor[2]
-        var l4 =document.getElementById('letter4_'+tentativa)
-            l4.innerHTML=palavra_vetor[3]
-        var l5 =document.getElementById('letter5_'+tentativa)
-            l5.innerHTML=palavra_vetor[4]
-        }
-        {
-            if( palavra_chave.includes(palavra_vetor[0])){
-                l1.className="letter s_right"
+        // var palavra = window.prompt('escolha a palavra')
+        var palavra = 'teste'
+        if (palavra.length!=5){
+            window.alert('palavra não conforme')
+        }else{
+            palavra_vetor=sprt_palavra(palavra)
+            {//atribuindo as letras no html  
+            var l1 = document.getElementById('letter1_'+tentativa)
+                l1.innerHTML=palavra_vetor[0]
+            var l2 =document.getElementById('letter2_'+tentativa)
+                l2.innerHTML=palavra_vetor[1]
+            var l3 =document.getElementById('letter3_'+tentativa)
+                l3.innerHTML=palavra_vetor[2]
+            var l4 =document.getElementById('letter4_'+tentativa)
+                l4.innerHTML=palavra_vetor[3]
+            var l5 =document.getElementById('letter5_'+tentativa)
+                l5.innerHTML=palavra_vetor[4]
+            }
+            {
+                if( palavra_chave.includes(palavra_vetor[0])){
+                    l1.className="letter s_right"
+                }else{
+                    l1.className="letter wrong"
+                }
+                if(palavra_vetor[0]==palavra_chave[0]){
+                    l1.className="letter right"
+                }
+                if(palavra_chave.includes(palavra_vetor[1])){
+                    l2.className="letter s_right"
+                }else{
+                    l2.className="letter wrong"
+                }
+                if(palavra_vetor[1]==palavra_chave[1]){
+                    l2.className="letter right"
+                }
+                if(palavra_chave.includes(palavra_vetor[2])){
+                    l3.className="letter s_right"
+                }else{
+                    l3.className="letter wrong"
+                }
+                if(palavra_vetor[2]==palavra_chave[2]){
+                    l3.className="letter right"
+                }
+                if(palavra_chave.includes(palavra_vetor[3])){
+                    l4.className="letter s_right"
+                }else{
+                    l4.className="letter wrong"
+                }
+                if(palavra_vetor[3]==palavra_chave[3]){
+                    l4.className="letter right"
+                }
+                if(palavra_chave.includes(palavra_vetor[4])){
+                    l5.className="letter s_right"
+                }else{
+                    l5.className="letter wrong"
+                }
+                if(palavra_vetor[4]==palavra_chave[4]){
+                    l5.className="letter right"
+                }
+            }
+            var  input_box = document.getElementById('input_box')
+            if (tentativa<6){
+                input_box.style.top = (8+(78*tentativa))+'px'
+                console.log(tentativa, " - " , ((90*tentativa))+'px')
             }else{
-                l1.className="letter rong"
+                input_box.style.display='none'
             }
-            if(palavra_vetor[0]==palavra_chave[0]){
-                l1.className="letter right"
-            }
-            if(palavra_chave.includes(palavra_vetor[1])){
-                l2.className="letter s_right"
-            }else{
-                l2.className="letter rong"
-            }
-            if(palavra_vetor[1]==palavra_chave[1]){
-                l2.className="letter right"
-            }
-            if(palavra_chave.includes(palavra_vetor[2])){
-                l3.className="letter s_right"
-            }else{
-                l3.className="letter rong"
-            }
-            if(palavra_vetor[2]==palavra_chave[2]){
-                l3.className="letter right"
-            }
-            if(palavra_chave.includes(palavra_vetor[3])){
-                l4.className="letter s_right"
-            }else{
-                l4.className="letter rong"
-            }
-            if(palavra_vetor[3]==palavra_chave[3]){
-                l4.className="letter right"
-            }
-            if(palavra_chave.includes(palavra_vetor[4])){
-                l5.className="letter s_right"
-            }else{
-                l5.className="letter rong"
-            }
-            if(palavra_vetor[4]==palavra_chave[4]){
-                l5.className="letter right"
-            }
-        }
-        
-        tentativa++
+    
+            tentativa++
+    }
         
     }
 }
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const input3 = document.getElementById("input3");
+const input4 = document.getElementById("input4");
+const input5 = document.getElementById("input5");
 
-cont(5,[1,2,3,4,5])
+input1.addEventListener("input", function () {
+    if (this.value.length === this.maxLength) {input2.focus();
+    }else if(this.value.length === 0){input1.focus();}});
+input2.addEventListener("input", function () {
+    if (this.value.length === this.maxLength) {input3.focus();
+    }else if(this.value.length === 0){input1.focus();}});
+input3.addEventListener("input", function () {
+    if (this.value.length === this.maxLength) {input4.focus();
+    }else if(this.value.length === 0){input2.focus();}});
+input4.addEventListener("input", function () {
+    if (this.value.length === this.maxLength) {input5.focus();
+    }else if(this.value.length === 0){input3.focus();}});
+input5.addEventListener("input", function () {
+    if (this.value.length === this.maxLength) {input5.focus();
+    }else if(this.value.length === 0){input4.focus();}});
