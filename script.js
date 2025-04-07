d9f9ce44c7a524e42d405b6044a79b097bb9dd59
Eliel-Julio <elieljuliosoaresdasilva@gmail.com>
2025-04-07 02:21:18 -0300

    .

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-var tentativa = 1
 // var palavra_chave = 'carne'
-
 const hoje = new Date().toISOString().split('T')[0];
-
 fetch("termos.txt")
     .then(response => response.text())
     .then(texto => {
-    // console.log("Conteúdo do arquivo:", texto);
-    const palavras = texto.split('\n')
-});
+    var tentativa = 1
+    palavras = texto.split('\r\n')
+
+    var rng = new Math.seedrandom(hoje);
+    var palavra_chave = palavras[Math.floor(rng() * palavras.length)]
+    console.log(' data:' ,hoje,
+                '\n resposta do dia:', palavra_chave)
 
 
-// var palavra_chave = palavras[Math.floor(seedrandom(hoje) * palavras.length)]
-console.log(hoje, '-' ,seedrandom(hoje))
-
-const input1 = document.getElementById("input1");
-const input2 = document.getElementById("input2");
-const input3 = document.getElementById("input3");
-const input4 = document.getElementById("input4");
-const input5 = document.getElementById("input5");
 
 const inputs = document.querySelectorAll("input");
 
-input1.focus()
+inputs[0].focus()
 
 function sprt_palavra(str){
     palavra_vet = [
@@ -34,109 +27,65 @@ function sprt_palavra(str){
     ]
     return palavra_vet
 }
-function contem(elemeto,vetor){
-    for (const i in vetor) {
-        if(i==elemeto){
-            return true
-            break
-    }
-    return false
-}
-}
+
 function get_palavra(){
     palavra_vetor=[
-        input1.value,
-        input2.value,
-        input3.value,
-        input4.value,
-        input5.value]
+        inputs[0].value,
+        inputs[1].value,
+        inputs[2].value,
+        inputs[3].value,
+        inputs[4].value]
     if (palavra_vetor.includes('')){
         window.alert('preencha todos os campos')
     }else{
         definir_palavra()
-        console.log(palavra_vetor, NaN)
-        input1.value=''
-        input2.value=''
-        input3.value=''
-        input4.value=''
-        input5.value=''
-        input1.focus()
+        console.log(" Input:",palavra_vetor)
+        inputs[0].value=''
+        inputs[1].value=''
+        inputs[2].value=''
+        inputs[3].value=''
+        inputs[4].value=''
+
+        inputs[0].focus()   
 }}
 function definir_palavra(){
     if(tentativa>6){
         window.alert("tentativas esgotadas")
-    }else{
+    }else if(palavras.includes(palavra_vetor.join(''))){
         // var palavra = window.prompt('escolha a palavra')
         // var palavra = 'teste'
-        if (0!=0 /*palavra.length!=5*/){
-            window.alert('palavra não conforme')
-        }else{
             // palavra_vetor=sprt_palavra(palavra)
-            {//atribuindo as letras no html  
-            var l1 = document.getElementById('letter1_'+tentativa)
-                l1.innerHTML=palavra_vetor[0]
-            var l2 =document.getElementById('letter2_'+tentativa)
-                l2.innerHTML=palavra_vetor[1]
-            var l3 =document.getElementById('letter3_'+tentativa)
-                l3.innerHTML=palavra_vetor[2]
-            var l4 =document.getElementById('letter4_'+tentativa)
-                l4.innerHTML=palavra_vetor[3]
-            var l5 =document.getElementById('letter5_'+tentativa)
-                l5.innerHTML=palavra_vetor[4]
-            }
-            {
-                if( palavra_chave.includes(palavra_vetor[0])){
-                    l1.className="letter s_right"
+            // //atribuindo as letras no html  
+            const letras = document.getElementById("line"+tentativa).querySelectorAll(".letter")
+            letras.forEach((letra, index) =>{
+                
+                letra.innerHTML = palavra_vetor[index];
+                
+                if( palavra_chave.includes(palavra_vetor[index])){
+                    letra.className="letter s_right"
                 }else{
-                    l1.className="letter wrong"
+                    letra.className="letter wrong"
                 }
-                if(palavra_vetor[0]==palavra_chave[0]){
-                    l1.className="letter right"
+                if(palavra_vetor[index]==palavra_chave[index]){
+                    letra.className="letter right"
                 }
-                if(palavra_chave.includes(palavra_vetor[1])){
-                    l2.className="letter s_right"
-                }else{
-                    l2.className="letter wrong"
-                }
-                if(palavra_vetor[1]==palavra_chave[1]){
-                    l2.className="letter right"
-                }
-                if(palavra_chave.includes(palavra_vetor[2])){
-                    l3.className="letter s_right"
-                }else{
-                    l3.className="letter wrong"
-                }
-                if(palavra_vetor[2]==palavra_chave[2]){
-                    l3.className="letter right"
-                }
-                if(palavra_chave.includes(palavra_vetor[3])){
-                    l4.className="letter s_right"
-                }else{
-                    l4.className="letter wrong"
-                }
-                if(palavra_vetor[3]==palavra_chave[3]){
-                    l4.className="letter right"
-                }
-                if(palavra_chave.includes(palavra_vetor[4])){
-                    l5.className="letter s_right"
-                }else{
-                    l5.className="letter wrong"
-                }
-                if(palavra_vetor[4]==palavra_chave[4]){
-                    l5.className="letter right"
-                }
-            }
+                })        
             var  input_box = document.getElementById('input_box')
             if (tentativa<6){
                 input_box.style.top = (8+(78*tentativa))+'px'
-                console.log(tentativa, " - " , ((90*tentativa))+'px')
+                console.log(" Tentativa: ",tentativa, 
+                            "\n posição:" , (8+(78*tentativa))+'px'
+                            )
+                // console.log('${tentativa} - ${input_box.style.top}')
             }else{
                 input_box.style.display='none'
             }
     
             tentativa++
-    }
+    
         
+    }else{
+        window.alert('palavra não existe')
     }
 }
 function focarProximoVazio(atualIndex) {
@@ -184,3 +133,4 @@ inputs.forEach((input, index) => {
 
 })
     
+})
