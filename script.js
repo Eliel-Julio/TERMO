b5d0b987cdb0bc18b2d30ad18a56c795f60ee20d
Eliel-Julio <elieljuliosoaresdasilva@gmail.com>
2025-04-07 02:45:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // var palavra_chave = 'carne'
 const hoje = new Date().toISOString().split('T')[0];
-fetch("palavras_termo.txt")
+fetch("termos.txt")
     .then(response => response.text())
     .then(texto => {
     var tentativa = 1
-    palavras = texto.split('\n')
+    palavras = texto.split('\r\n')
 
     var rng = new Math.seedrandom(hoje);
     var palavra_chave = palavras[Math.floor(rng() * palavras.length)]
